869d846845b899bf56e6162b97fe26f9c06c5880
yesrab <yesrab.ali111@gmail.com>
2023-10-01 16:18:56 +0530

    added clac functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const arr = ["-47", "69", "+", "11", "*", "79", "/"];
+const arr = ["33", "29", "*", "4", "+"];
 
 console.log(evalRPN(arr));
 
@@ -50,62 +50,73 @@ let btns = document.querySelectorAll("button");
 
 for (i of btns) {
   i.addEventListener("click", function () {
-    // console.log(this.id);
+    // console.log("val", this.value);
     switch (this.id) {
       case "num1": {
         console.log("number 1 btn");
         displayArea.value += "1";
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "num2": {
         console.log("number 2 btn");
         displayArea.value += "2";
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "num3": {
         console.log("number 3 btn");
         displayArea.value += "3";
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "num4": {
         console.log("number 4 btn");
         displayArea.value += "4";
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "num5": {
         console.log("number 5 btn");
         displayArea.value += "5";
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "num6": {
         console.log("number 6 btn");
         displayArea.value += "6";
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "num7": {
         console.log("number 7 btn");
         displayArea.value += "7";
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "num8": {
         console.log("number 8 btn");
         displayArea.value += "8";
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "num9": {
         console.log("number 9 btn");
         displayArea.value += "9";
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "num0": {
         console.log("number 0 btn");
         displayArea.value += "0";
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "addBtn": {
         console.log("+ operator");
         if (displayArea.value.trim() !== "") {
           displayArea.value += "+";
+          displayArea.scrollLeft = 100000;
         }
         break;
       }
@@ -113,6 +124,7 @@ for (i of btns) {
         console.log("- operator");
         if (displayArea.value.trim() !== "") {
           displayArea.value += "-";
+          displayArea.scrollLeft = 100000;
         }
         break;
       }
@@ -120,6 +132,7 @@ for (i of btns) {
         console.log("* operator");
         if (displayArea.value.trim() !== "") {
           displayArea.value += "*";
+          displayArea.scrollLeft = 100000;
         }
         break;
       }
@@ -127,6 +140,7 @@ for (i of btns) {
         console.log("/ operator");
         if (displayArea.value.trim() !== "") {
           displayArea.value += "/";
+          displayArea.scrollLeft = 100000;
         }
         break;
       }
@@ -134,12 +148,18 @@ for (i of btns) {
         console.log(". button");
         if (displayArea.value.trim() !== "") {
           displayArea.value += ".";
+          displayArea.scrollLeft = 100000;
         }
         break;
       }
       case "delBtn": {
         console.log("Del button");
-        displayArea.value = displayArea.value.slice(0, -1);
+        if (displayArea.value == "Infinity" || displayArea.value == "NaN") {
+          displayArea.value = "";
+        } else {
+          displayArea.value = displayArea.value.slice(0, -1);
+        }
+        displayArea.scrollLeft = 100000;
         break;
       }
       case "resetBtn": {
@@ -151,19 +171,41 @@ for (i of btns) {
         console.log("= button");
         // console.log(eval(displayArea.value));
         console.log(parseString(displayArea.value));
-        const newarr = parseString(displayArea.value);
-        console.log(validateExpression(newarr));
-
+        const validOut = parseString(displayArea.value);
+        console.log(validateExpression(validOut));
+        if (validateExpression(validOut)) {
+          displayArea.value = evalRPN(SYA(validOut));
+        } else {
+          displayArea.value = "invalid expression";
+        }
+        displayArea.scrollLeft = 100000;
         break;
       }
     }
   });
 }
+// function parseString(string) {
+//   const regex = /(\d+(\.\d*)?|\.\d+|[+\-*/])/g;
+//   const matches = string.match(regex);
+//   return matches || [];
+// }
+// added handling of negitive numbers
 function parseString(string) {
-  const regex = /(\d+(\.\d*)?|\.\d+|[+\-*/])/g;
-  const matches = string.match(regex);
-  return matches || [];
+  const pattern = /([-+*/])|([0-9.]+)/g;
+  const result = string.match(pattern) || [];
+  for (let i = 0; i < result.length; i++) {
+    if (
+      result[i] === "-" &&
+      (i === 0 || ["+", "-", "*", "/"].includes(result[i - 1]))
+    ) {
+      result[i] += result[i + 1];
+      result.splice(i + 1, 1);
+    }
+  }
+
+  return result;
 }
+
 function getType(element) {
   if (
     element === "+" ||
@@ -191,4 +233,35 @@ function validateExpression(arr) {
   }
 
   return true;
+}
+
+function SYA(array) {
+  const stack = [];
+  const queue = [];
+  const operators = {
+    "*": 2,
+    "/": 2,
+    "+": 1,
+    "-": 1,
+  };
+
+  for (const element of array) {
+    if (getType(element) === "number") {
+      queue.push(element);
+    } else if (getType(element) === "operator") {
+      while (
+        stack.length > 0 &&
+        operators[stack[stack.length - 1]] >= operators[element]
+      ) {
+        queue.push(stack.pop());
+      }
+      stack.push(element);
+    }
+  }
+  while (stack.length > 0) {
+    queue.push(stack.pop());
+  }
+
+  console.log(queue);
+  return queue;
 }
